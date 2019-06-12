fa537415e8ee0b8f19017bfbfa60bd71f513647f
gsterrell <gsterrell@gmail.com>
2019-06-12 01:36:12 -0400

    Added very basic collision detection, stop if you run into blocks, but fall through it if you jump on it. Also made lame Player and Block classes until we figure out more of what we want from them. I also changed some code styling to what Pycharm suggested so we are more Python-ish (even if I do not like all the suggestions personally)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.init()
 
 path = "./characters/Jill/"
 
-size = width, height = 1600, 900
+size = width, height = 1280, 720
 black = 0, 0, 0
 white = 255, 255, 255
 
@@ -18,6 +18,9 @@ jump_distance = 0
 face_direction = 1
 #0: Left  1: Right
 
+sprite_coordinates = [0, height-48]
+block_coordinates = [200, height-48]
+
 screen = pygame.display.set_mode(size)
 pygame.mixer.music.load("BGM.wav")
 #Patakas World
@@ -26,14 +29,53 @@ pygame.mixer.music.load("BGM.wav")
 pygame.mixer.music.play(-1)
 
 pygame.display.set_caption("Puzzle Box")
-character = pygame.image.load(path + "Right.gif")
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.image.load(path + "Right.gif")
+        self.rect = self.img.get_rect()
+        self.coordinates = 0, 0
+        self.rect.x, self.rect.y = self.coordinates
+
+    def check_collision(self, sprite1, sprite2):
+        collided = pygame.sprite.collide_rect(sprite1, sprite2)
+        if collided and self.rect.x <= sprite2.rect.x:
+            self.update_position(-1, 0)
+        elif collided and self.rect.x >= sprite2.rect.x:
+            self.update_position(1, 0)
+
+    def update_position(self, offset_x, offset_y):
+        self.rect.x += offset_x
+        self.rect.y += offset_y
+        self.coordinates = self.rect.x, self.rect.y
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.image.load("./sprites/block24x.png")
+        self.rect = self.img.get_rect()
+        self.coordinates = 0, 0
+        self.rect.x, self.rect.y = self.coordinates
+
+    def update_position(self, offset_x, offset_y):
+        self.rect.x += offset_x
+        self.rect.y += offset_y
+        self.coordinates = self.rect.x, self.rect.y
+
 
 # Source: opengameart.org
 # Name from source: Sara and Star
 # Artist: Mandi Paugh
 
 
-sprite_coordinates = [0, 850]
+player = Player()
+player.update_position(0, height-48)
+
+block = Block()
+block.update_position(200, height-24)
 
 while 1:
     for event in pygame.event.get():
@@ -50,42 +92,45 @@ while 1:
                     path = "./characters/Jill/"
 
                 if face_direction == 0:
-                    character = pygame.image.load(path + "Left.gif")
+                    player.img = pygame.image.load(path + "Left.gif")
                 elif face_direction == 1:
-                    character = pygame.image.load(path + "Right.gif")
+                    player.img = pygame.image.load(path + "Right.gif")
 
     screen.fill(black)
     keys = pygame.key.get_pressed()
-    if keys[K_a] == True:
-        sprite_coordinates[0] -= 1
+    if keys[K_a]:
+        player.update_position(-1, 0)
         face_direction = 0
-        character = pygame.image.load(path + "Left.gif")
-        if sprite_coordinates[0] == -1:
-            sprite_coordinates[0] = 0
+        player.img = pygame.image.load(path + "Left.gif")
+        if player.coordinates[0] == -1:
+            player.update_position(1, 0)
 
-    elif keys[K_d] == True:
-        sprite_coordinates[0] += 1
+    elif keys[K_d]:
+        player.update_position(1, 0)
         face_direction = 1
-        character = pygame.image.load(path + "Right.gif")
-        if sprite_coordinates[0] == 1568:
-            sprite_coordinates[0] = 1567
+        player.img = pygame.image.load(path + "Right.gif")
+        if player.coordinates[0] == width-32:
+            player.update_position(-1, 0)
 
-    if keys[K_j] == True and jumping == False and falling == False:
+    if keys[K_j] and not jumping and not falling:
         jumping = True
 
-    if jumping == True:
-        sprite_coordinates[1] -= 1
+    if jumping:
+        player.update_position(0, -1)
         jump_distance += 1
-    elif falling == True:
-        sprite_coordinates[1] += 1
+    elif falling:
+        player.update_position(0, 1)
         jump_distance -= 1
 
     if jump_distance == jump_height:
         jumping = False
         falling = True
-    if jump_distance == 0 and falling == True:
+    if jump_distance == 0 and falling:
         falling = False
 
+    player.check_collision(player, block)
+
     pygame.draw.line(screen, white, (1, 899), (1599, 899), 5)
-    screen.blit(character, sprite_coordinates)
+    screen.blit(player.img, player.coordinates)
+    screen.blit(block.img, block_coordinates)
     pygame.display.flip()
