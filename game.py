67b5e2fa8f94a6fafede34f8addfcb2b520ca11f
guy6020665 <guy6020665@gmail.com>
2019-06-15 08:39:40 -0400

    Moved Jumping and Falling code to Player class
    Also attempted to add code to detect landing on a block. Seems to work, get pushed off the block too quickly to tell though.

--- game.py
+++ game.py
@@ -13,10 +13,7 @@ def game():
     black = 0, 0, 0
     white = 255, 255, 255
 
-    jump_height = 75
     jumping = False
-    falling = False
-    jump_distance = 0
     face_direction = 1
     # 0: Left  1: Right
 
@@ -42,10 +39,20 @@ def game():
             self.rect = self.img.get_rect()
             self.coordinates = 0, 0
             self.rect.x, self.rect.y = self.coordinates
+            self.jumping = False
+            self.falling = False
+            self.jump_distance = 0
+            self.jump_height = 75
+
 
         def check_collision(self, sprite1, sprite2):
             collided = pygame.sprite.collide_rect(sprite1, sprite2)
-            if collided and self.rect.x <= sprite2.rect.x:
+            if collided and self.rect.y < sprite2.rect.y and self.falling == True:
+                # using this as there is no other instance where both should be true at the same time
+                self.jumping = True
+                self.jump_distance = 0
+
+            elif collided and self.rect.x <= sprite2.rect.x:
                 self.update_position(-5, 0)
             elif collided and self.rect.x >= sprite2.rect.x:
                 self.update_position(5, 0)
@@ -54,6 +61,36 @@ def game():
             self.rect.x += offset_x
             self.rect.y += offset_y
             self.coordinates = self.rect.x, self.rect.y
+
+        def jump(self):
+            if self.falling == True:
+                self.fall()
+            elif self.jump_distance > self.jump_height:
+                self.jumping = False
+                self.falling = True
+                self.fall()
+            elif not self.jumping:
+                self.jumping = True
+            if self.jumping and not self.falling:
+                self.update_position(0, -3)
+                self.jump_distance += 3
+                return True
+
+        def fall(self):
+
+            if pygame.sprite.collide_rect(self, block) and self.rect.y < block.rect.y - 40:
+                self.falling = False
+                self.jump_distance = 0
+            elif self.rect.y > height - 48:
+                self.rect.y = height - 48
+                self.jump_distance = 0
+                self.falling = False
+            else:
+                self.update_position(0,3)
+                self.falling = True
+                return
+
+
 
     class Block(pygame.sprite.Sprite):
         def __init__(self):
@@ -97,6 +134,9 @@ def game():
                     elif face_direction == 1:
                         player.img = pygame.image.load(path + "Right.gif")
 
+                elif event.key == K_j:
+                    jumping = True
+
         screen.fill(black)
         keys = pygame.key.get_pressed()
         if keys[K_a]:
@@ -113,21 +153,10 @@ def game():
             if player.coordinates[0] >= width - 32:
                 player.update_position(-5, 0)
 
-        if keys[K_j] and not jumping and not falling:
-            jumping = True
-
-        if jumping:
-            player.update_position(0, -3)
-            jump_distance += 3
-        elif falling:
-            player.update_position(0, 3)
-            jump_distance -= 3
-
-        if jump_distance == jump_height:
-            jumping = False
-            falling = True
-        if jump_distance == 0 and falling:
-            falling = False
+        if keys[K_j]:
+            player.jump()
+        elif not keys[K_j]:
+            player.fall()
 
         player.check_collision(player, block)
 
