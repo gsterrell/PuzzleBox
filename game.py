1e776106aa0b5f5b95f2b63b2be4f7c442f5a76f
ArtirHyseni <51412936+ArtirHyseni@users.noreply.github.com>
2019-06-17 11:00:41 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,73 @@
 import pygame
 import sys
+import time
+
+from pygame.locals import *
+
+pygame.init()
+width = 1280
+height = 720
+black = 0, 0, 0
+white = 255, 255, 255
+red = 200,0,0
+green = 0,200,0
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+backgroundIntro = 56,142,142
+gameDisplay = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Puzzle Game')
+clock = pygame.time.Clock()
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    #print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects("Puzzle Game", largeText)
+    TextRect.center = ((width / 2), (height / 2))
+    gameDisplay.blit(TextSurf, TextRect)
+    pygame.display.update()
+    time.sleep(2)
+    game()
+
+def game_intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        gameDisplay.fill(backgroundIntro)
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("Puzzle Game", largeText)
+        TextRect.center = ((width/2),(height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        button("Start", 600, 450, 100, 50, green, bright_green, game)
+        button("Quit", 600, 550, 100, 50, red, bright_red, quitgame)
+        pygame.display.update()
+        clock.tick(15)
+
+def quitgame():
+    quit()
+
 
 from pygame.locals import *
 
